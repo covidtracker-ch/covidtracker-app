f781fa81f25ef30204d5b6df605052ac503de9b4
Jonathan Lehner <lehnerj@student.ethz.ch>
2020-04-26 03:28:42 +0200

    cut off language bar and added android

--- src/pages/Questionnaire.js
+++ src/pages/Questionnaire.js
@@ -9,13 +9,7 @@ export default class extends React.Component {
     render() {
       return (
         <Page title="" menu style={{overflow: 'hidden', position: 'relative', width: '100%', height: '100%'}}>
-          <iframe width="100%"
-            style={{
-              position: 'absolute',
-              height: '100%',
-              border: 0,
-              width: '100%'
-            }} 
+          <iframe id="main_iframe" 
             src="https://covidtracker.ch"></iframe>
         </Page>
       );
