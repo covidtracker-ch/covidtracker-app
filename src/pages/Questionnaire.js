d3f04dc6eca890bf3be2344d7291e252f3a305c3
Jonathan Lehner <lehnerj@student.ethz.ch>
2020-04-26 04:36:32 +0200

    adjusted to website

--- src/pages/Questionnaire.js
+++ src/pages/Questionnaire.js
@@ -5,12 +5,14 @@ import Page from '../components/Page';
 
 
 export default class extends React.Component {
-        
+
     render() {
+      const isLocal = false;
+      const src = isLocal ? "http://10.1.1.60:4567/":"https://covidtracker.ch/";  
       return (
         <Page title="" menu style={{overflow: 'hidden', position: 'relative', width: '100%', height: '100%'}}>
           <iframe id="main_iframe" 
-            src="https://covidtracker.ch"></iframe>
+            src={src}></iframe>
         </Page>
       );
     }
