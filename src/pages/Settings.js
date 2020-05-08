4f0eeba953bd8c0e828d09979f93802390b2413b
Jonathan Lehner <lehnerj@student.ethz.ch>
2020-05-08 02:09:26 +0200

    added UI for setting notification time

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -2,12 +2,20 @@
 import React from 'react';
 import * as Ion from '@ionic/react';
 import Page from '../components/Page';
+import TimePickerWrapper from 'react-times';
+import { IonToggle, IonItem, IonLabel } from '@ionic/react';
 
+// or you can use classic theme
+import 'react-times/css/classic/default.css';
 
 export default class extends React.Component {
     constructor(){
       super()
-      this.state = {language: this.getLanguage()}      
+      this.state = {
+        language: this.getLanguage(),
+        notificationTime: this.getNotificationTime(),
+        allowNotifications: this.getAllowNotifications()
+      }      
     }
 
     setLanguage(event, languageCode){
@@ -18,6 +26,12 @@ export default class extends React.Component {
     }
     getLanguage = () => {
       return localStorage.getItem("language") || "";
+    }
+    getNotificationTime = () => {
+      return localStorage.getItem("notificationTime") || "17:00";
+    }
+    getAllowNotifications = () => {
+      return localStorage.getItem("allowNotifications") || true;
     }
 
     render() {
@@ -38,7 +52,34 @@ export default class extends React.Component {
             <li>
               <a onClick={(e)=>this.setLanguage(e, "en")} style={language === "en" ? {color: "white", background: "red"} : {}} href="/#">English</a>
             </li>
-          </ul>          
+          </ul>  
+          <p>Benachrichtigungen einschalten:</p>
+          <div style={{width: "200px", margin: "20px"}}>
+            <IonToggle checked={this.state.allowNotifications} onIonChange={
+              e => {
+                this.setState({allowNotifications: e.detail.checked});
+                localStorage.setItem("allowNotifications", e.detail.checked);
+                }
+              } />
+          </div>
+          {this.state.allowNotifications ? 
+            <div>
+              <p>Jeden Tag:</p>
+              <div style={{width: "200px"}}>
+                <TimePickerWrapper 
+                  theme="classic" 
+                  time={this.state.notificationTime}
+                  onTimeChange={(options)=>{ // you can get hour, minute and meridiem here
+                    const { hour, minute, meridiem } = options; 
+                    console.log(options);
+                    this.setState({notificationTime: `${hour}:${minute}`});
+                    localStorage.setItem("notificationTime", `${hour}:${minute}`);
+                    }
+                  }
+                />   
+              </div>
+            </div>
+            : ""}
         </Page>
       );
     }
